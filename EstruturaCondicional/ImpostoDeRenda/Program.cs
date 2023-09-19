b0facd22fb95685c7fe98f4ac53f79effb6de97c
Ítalo Penha <italopenha77@outlook.com>
2023-09-18 21:34:18 -0300

    Adicionando exercícios

---
+++ EstruturaCondicional/ImpostoDeRenda/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ImpostoDeRenda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double impostoFinal, valorBase, faixa008, faixa018, faixa028;
+
+            double salario = float.Parse(Console.ReadLine());
+
+            switch (salario)
+            {
+                case < 2000.00:
+                    Console.WriteLine("Isento");
+                    break;
+                case < 3000.00:
+                    impostoFinal = (salario - 2000.00) * 0.08;
+                    Console.WriteLine($"R$ {impostoFinal.ToString("F2")}");
+                    break;
+                case < 4500.00:
+                    valorBase = (salario - 2000.00);
+                    faixa008 = 1000 * 0.08;
+                    faixa018 = (valorBase - 1000) * 0.18;
+                    impostoFinal = faixa008 + faixa018;
+                    Console.WriteLine($"R$ {impostoFinal.ToString("F2")}");
+                    break;
+                case > 4500.00:
+                    valorBase = (salario - 2000.00);
+                    faixa008 = 1000 * 0.08;
+                    faixa018 = 1500 * 0.18;
+                    faixa028 = (valorBase - 2500) * 0.28;
+                    impostoFinal = faixa008 + faixa018 + faixa028;
+                    Console.WriteLine($"R$ {impostoFinal.ToString("F2")}");
+                    break;
+            }
+        }
+    }
+}
---
+++ EstruturaCondicional/SortSimples/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace SortSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int a = int.Parse(valores[0]);
+            int b = int.Parse(valores[1]);
+            int c = int.Parse(valores[2]);
+
+            int n1, n2, n3;
+
+            if (a < b && a < c)
+            {
+                n1 = a;
+                if (b < c)
+                {
+                    n2 = b;
+                    n3 = c;
+                    Console.WriteLine(n1);
+                    Console.WriteLine(n2);
+                    Console.WriteLine(n3);
+                }
+                else if (c < b)
+                {
+                    n2 = c;
+                    n3 = b;
+                    Console.WriteLine(n1);
+                    Console.WriteLine(n2);
+                    Console.WriteLine(n3);
+                }
+            }
+            else if (b < c && b < a)
+            {
+                n1 = b;
+                if (a < c)
+                {
+                    n2 = a;
+                    n3 = c;
+                    Console.WriteLine(n1);
+                    Console.WriteLine(n2);
+                    Console.WriteLine(n3);
+                }
+                else if (c < a)
+                {
+                    n2 = c;
+                    n3 = a;
+                    Console.WriteLine(n1);
+                    Console.WriteLine(n2);
+                    Console.WriteLine(n3);
+                }
+            }
+            else if (c < a && c < b)
+            {
+                n1 = c;
+                if (a < b)
+                {
+                    n2 = a;
+                    n3 = b;
+                    Console.WriteLine(n1);
+                    Console.WriteLine(n2);
+                    Console.WriteLine(n3);
+                }
+                else if (b < a)
+                {
+                    n2 = b;
+                    n3 = a;
+                    Console.WriteLine(n1);
+                    Console.WriteLine(n2);
+                    Console.WriteLine(n3);
+                }
+            }
+
+            Console.WriteLine("");
+
+            Console.WriteLine(valores[0]);
+            Console.WriteLine(valores[1]);
+            Console.WriteLine(valores[2]);
+        }
+    }
+}
