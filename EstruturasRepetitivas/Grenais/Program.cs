8780e905bea6fdbe88812f9ea42d8fa13349e001
Ítalo Penha <italopenha77@outlook.com>
2023-09-20 22:15:28 -0300

    Adicionando exercícios

---
+++ EstruturasRepetitivas/Grenais/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Grenais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vInter = 0, vGremio = 0, empates = 0, grenais = 0, gInter, gGremio, codigo = 1;
+            
+            do
+            {
+                string[] gols = Console.ReadLine().Split(' ');
+                gInter = int.Parse(gols[0]);
+                gGremio = int.Parse(gols[1]);
+
+                if (gInter > gGremio)
+                {
+                    vInter++;
+                    grenais++;
+                }
+                else if (gGremio > gInter)
+                {
+                    vGremio++;
+                    grenais++;
+                }
+                else
+                {
+                    empates++;
+                    grenais++;
+                }
+
+                Console.WriteLine("Novo grenal (1-sim 2-nao)");
+                codigo = int.Parse(Console.ReadLine());
+            } while (codigo == 1);
+
+            Console.WriteLine($"{grenais} grenais");
+            Console.WriteLine($"Inter:{vInter}");
+            Console.WriteLine($"Gremio:{vGremio}");
+            Console.WriteLine($"Empates:{empates}");
+
+            if (vInter > vGremio)
+                Console.WriteLine("Inter venceu mais");
+            else if (vGremio > vInter)
+                Console.WriteLine("Gremio venceu mais");
+            else
+                Console.WriteLine("Nao houve vencedor");
+        }
+    }
+}
---
+++ EstruturasRepetitivas/VariasNotasComValidacao/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace VariasNotasComValidacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigo = 1;
+
+            do
+            {
+                int cont = 0;
+                double nota = 0.0, somaNotas = 0.0, media;
+                while (cont < 2)
+                {
+                    nota = double.Parse(Console.ReadLine());
+                    if (nota < 0.0 || nota > 10.0)
+                    {
+                        Console.WriteLine("nota invalida");
+                    }
+                    else
+                    {
+                        somaNotas += nota;
+                        cont++;
+                    }
+                }
+                media = somaNotas / cont;
+                Console.WriteLine($"media = {media.ToString("F2")}");
+
+                do
+                {
+                    Console.WriteLine("novo calculo (1-sim 2-nao)");
+                    codigo = int.Parse(Console.ReadLine());
+                } while (codigo < 1 || codigo > 2);
+
+            } while (codigo == 1);
+        }
+    }
+}
