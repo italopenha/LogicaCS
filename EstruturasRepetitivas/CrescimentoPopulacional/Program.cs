c4cc4dc4aaa1d0e3c0e948b355d0f41b4fb3265c
Ítalo Penha <italopenha77@outlook.com>
2023-09-27 22:09:02 -0300

    Adicionando exercícios

---
+++ EstruturasRepetitivas/CrescimentoPopulacional/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CrescimentoPopulacional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                int pa = int.Parse(valores[0]);
+                int pb = int.Parse(valores[1]);
+                decimal g1 = decimal.Parse(valores[2]);
+                decimal g2 = decimal.Parse(valores[3]);
+
+                int pTotalA = pa;
+                int pTotalB = pb;
+                int anos;
+
+                for (anos = 0; pTotalA <= pTotalB; anos++)
+                {
+                    pTotalA += (int)(pTotalA * (g1 / 100));
+
+                    pTotalB += (int)(pTotalB * (g2 / 100));
+                }
+
+                if (anos > 100)
+                    Console.WriteLine("Mais de 1 seculo.");
+                else
+                    Console.WriteLine($"{anos} anos.");
+            }
+        }
+    }
+}
---
+++ EstruturasRepetitivas/NumeroPrimo/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace NumeroPrimo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+
+                int metade = x / 2;
+                bool primo = true;
+                
+                for (int j = 2; j <= metade; j++)
+                {
+                    if (x % j == 0)
+                    {
+                        primo = false; break;
+                    }
+                }
+
+                if (primo)
+                {
+                    Console.WriteLine($"{x} eh primo");
+                }
+                else
+                {
+                    Console.WriteLine($"{x} nao eh primo");
+                }
+            }
+        }
+    }
+}
