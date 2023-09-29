a74afe625381cdae35e05e6d22fbaeda73b9abee
Ítalo Penha <italopenha77@outlook.com>
2023-09-28 21:53:09 -0300

    Adicionando exercícios

---
+++ Vetores/MaiorElemento/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MaiorElemento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double[] vet = new double[n];
+            string[] valores = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(valores[i]);
+            }
+
+            double maior = 0;
+            int posicaoMaior = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (vet[i] > maior)
+                {
+                    maior = vet[i];
+                    posicaoMaior = i;
+                }
+            }
+
+            Console.WriteLine(maior.ToString("F2"));
+            Console.WriteLine(posicaoMaior);
+        }
+    }
+}
---
+++ Vetores/SomaVetores/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SomaVetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de elementos que terão os vetores:");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Digite os {n} elementos do vetor A separados por um espaço:");
+            string[] valoresA = Console.ReadLine().Split(' ');
+
+            Console.WriteLine($"Digite os {n} elementos do vetor B separados por um espaço:");
+            string[] valoresB = Console.ReadLine().Split(' ');
+
+            int[] vetA = new int[n];
+            int[] vetB = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vetA[i] = int.Parse(valoresA[i]);
+                vetB[i] = int.Parse(valoresB[i]);
+            }
+
+            int[] vetC = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vetC[i] = vetA[i] += vetB[i];
+            }
+
+            foreach (int i in vetC)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
